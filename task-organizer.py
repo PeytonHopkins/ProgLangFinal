53586ff023293f0f90a25adfabb68e92dc9f8538
Peyton Hopkins <peytonlukehopkins@outlook.com>
2023-12-13 13:09:55 -0600

    updates

--- task-organizer.py
+++ task-organizer.py
@@ -1,49 +1,64 @@
 # Peyton Hopkins
-#
-#
 
 import sys
+
 
 class Node:
     def __init__(self, task, dependentTask):
         self.task = task
         self.dependentTask = dependentTask
+        self.visited = False
         self.next = None
 
-    def next_node(self, next):
-        self.next = next
 
-print("Please input task values. (Use Ctrl+D to submit.)")
-tasksStandardInput = sys.__stdin__.readlines()  # Take users multiline input of tasks & dependent tasks
+def topSort(node):
+    node.visited = True
+    for checkNode in Nodes:
+        if checkNode.dependentTask == node.task and not checkNode.visited:
+            topSort(checkNode)
+            stack.append(node)
+
+
+Nodes = []
+
+debug = True
+
+if not debug:
+    print("Please input task values. (Use Ctrl+D to submit.)")
+    tasksStandardInput = sys.__stdin__.readlines()  # Take users multiline input of tasks & dependent tasks
+else:
+    file = open('testcase.txt', 'r')
+    tasksStandardInput = file.readlines()
+
 
 if tasksStandardInput.__len__() == 0:    # Check for 0 or non-even num of lines to ensure input is correct
-    print("ERROR: No lines detected.")
+    print("\033[31mERROR: No lines detected.")
     sys.exit()
-if tasksStandardInput % 2 != 0:
-    print("ERROR: Odd number of lines detected.")
+if tasksStandardInput.__len__() % 2 != 0:
+    print("\033[31mERROR: Odd number of lines detected.")
     sys.exit()
 
 isEven = False
 prevLine = ""
+
 for line in tasksStandardInput:
-    line = line[:-1]  # Remove the newline char
+    if '\n' in line:    # Remove the newline char if it is there
+        line = line[:-1]
     if isEven:  # dependent task
-        newNode = Node(line, prevLine)# Create Node
-    else:       # task
-       prevLine = line
+        newNode = Node(prevLine, line)  # Create Node (Task,Dependent Task)
+        Nodes.append(newNode)   # add new task to a list containing all the task & dependent task pairs
+    else:   # task
+        prevLine = line
 
-    isEven =  not isEven
+    isEven = not isEven
 
+stack = []
+visited = [False for i in range(Nodes.__len__())]
 
-    # Sort the taskStandardInput into task & dependent task (dictionary?) (Task,Dependent Task)
-    # Could be a "node" rather than a dictionary, where it can point to another node?
+index = 0
+for node in Nodes:
+    if not node.visited:
+        topSort(node)
 
-        # if line is odd, it is a task
-        # if line is even, it is dependent task
-        # add new task to a list containing all the task & dependent task pairs
-
-# Figure out starting point of the task list
-    # We must find which task is only a dependent task?
-    # Make it a head node for a new list?
-
-
+for node in stack:
+    print(node.task, node.dependentTask)
